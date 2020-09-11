0a3834b28d41f0b9e140983c0179454a86f08d6c
Juan <69590591+TtvNekix@users.noreply.github.com>
2020-09-11 22:05:57 +0200

    Add files via upload

---
+++ esx_watermarkV2/__resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page 'marca.html'
+
+
+files {
+    'marca.html',
+    'MyLogo.png'
+}
+
+client_script 'client.lua'
+
---
+++ esx_watermarkV2/client.lua
@@ -0,0 +1,1 @@
+SetNuiFocus(true, false)
